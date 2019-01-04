31559d8a50335b4ee4837d3205af2d44fcc59f12
Guo_ShiXin <18337177020@163.com>
2019-01-04 17:36:01 +0800

    集成swagger

---
+++ src/main/java/com/example/DF_Information/Swagger2.java
@@ -0,0 +1,9 @@
+package com.example.DF_Information;
+
+/**
+ * @Auther: guo
+ * @Date: 2019/1/4 17:06
+ * @Description:
+ */
+public class Swagger2 {
+}
---
+++ src/main/java/com/example/DF_Information/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.example.DF_Information.controller;
+
+/**
+ * @Auther: guo
+ * @Date: 2019/1/4 16:52
+ * @Description:
+ */
+public class TestController {
+}
