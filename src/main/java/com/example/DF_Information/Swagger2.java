aa6e9c7e4c66d239d1710f4c0914d995e01260f4
Guo_ShiXin <18337177020@163.com>
2019-01-04 17:39:20 +0800

    集成swagger

--- src/main/java/com/example/DF_Information/Swagger2.java
+++ src/main/java/com/example/DF_Information/Swagger2.java
@@ -1,9 +1,44 @@
 package com.example.DF_Information;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
- * @Auther: guo
+ * @Auther: guoshihao
  * @Date: 2019/1/4 17:06
- * @Description:
+ * @Description:swagger2启动类
  */
+@Configuration
+@EnableSwagger2
 public class Swagger2 {
+    //swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.DF_Information"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DF_Information  API")
+                .description("快速上手,快速开发,快速交接")
+                .contact(new Contact("GSH", "https://my.oschina.net/u/3681868/home", "15136390655@163.COM"))
+                .version("1.0.0")
+                .build();
+    }
+
+
 }
--- src/main/java/com/example/DF_Information/controller/TestController.java
+++ src/main/java/com/example/DF_Information/controller/TestController.java
@@ -1,9 +1,24 @@
 package com.example.DF_Information.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * @Auther: guo
+ * @Auther: guoshihao
  * @Date: 2019/1/4 16:52
- * @Description:
+ * @Description:测试类
  */
+@RestController
+@RequestMapping("/test")
+@Api("测试类相关的api")
 public class TestController {
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个的学生信息")
+    public String welcome() {
+        return "Welcome!";
+    }
 }
