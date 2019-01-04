bcd1b924c3eb8733552fb51069f312efba5318eb
guoshihao <15136390655@163.com>
2019-01-04 17:43:52 +0800

    springBoot启动类添加注释

--- src/main/java/com/example/DF_Information/DfInformationApplication.java
+++ src/main/java/com/example/DF_Information/DfInformationApplication.java
@@ -8,6 +8,15 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class DfInformationApplication {
 
+	/**
+	 *
+	 * @Description:springboot启动方法
+	 *
+	 * @param: [args]
+	 * @return: void
+	 * @auther: guoshihao
+	 * @date: 2019/1/4 17:43
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DfInformationApplication.class, args);
 	}
