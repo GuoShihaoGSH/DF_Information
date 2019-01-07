7d6aba9efeeffddd4bc956cbb34f575fa90b1de4
guoshihao <15136390655@163.com>
2019-01-07 17:10:06 +0800

    添加子工程

--- DF_web/src/main/java/com.example.DF_Information/controller/TestController.java
+++ DF_web/src/main/java/com.example.DF_Information/controller/TestController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("测试类相关的api")
 public class TestController {
 
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个的学生信息")
     public String welcome() {
